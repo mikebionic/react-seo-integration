70116aba97f8e8438317d4497db1762a3257390e
merri_23 <mekanhodzaberdiev@gmail.com>
2021-08-05 18:38:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express')
+
+var routes_list = require('./page_data')
+
+var app = express()
+
+
+app.set('view engine', 'ejs')
+app.use('/static', express.static('static'))
+
+app.listen(8080);
+
+app.get('/', function(req,res) {
+  var seo_response = get_page_data()
+  res.render('index', {seo_data: seo_response})
+})
+
+const get_page_data = (page_route_name = '') => {
+
+  var seo_response = {
+    "title": "Sap Çözgüt | Sap Hasap",
+    "description": "Söwda awtomatlaşdyrma, Ynamdar hyzmat, Döwrebap internet sahypalary, Mobile programma üpjünçiligi"
+  };
+
+  console.log(routes_list)
+  routes_list.map((r) => {
+    if (r["path"] === page_route_name.toString()){
+      seo_response["title"] = r["title"]
+      seo_response["description"] = r["description"]
+    }
+  })
+  return seo_response;
+}
+
+app.get('/:route', function(req,res){
+  var seo_response = get_page_data(req.params.route)
+  res.render('index', {seo_data: seo_response})
+})
+
+app.get('/locales/:lang/:filename', function(req,res){
+  console.log(__dirname)
+  var lang = req.params.lang
+  var filename = req.params.filename
+  console.log("requested locale")
+  console.log(__dirname)
+  res.sendFile(`${__dirname}/static/locales/${lang}/${filename}`)
+})
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});    
+
---
+++ page_data.js
@@ -0,0 +1,93 @@
+
+var routes_list = [
+  {
+    "path": "/",
+    "title": "Sap Hasap | Sap Çözgüt",
+    "description": "Söwda awtomatlaşdyrma, Ynamdar hyzmat, Döwrebap internet sahypalary, Mobile programma üpjünçiligi",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com",
+    "type": "website"
+    // "img": "./static/media/og_SapCozgut"
+  },
+  {
+    "path": "sapcozgut",
+    "title": "Baş sahypa",
+    "description": "Söwda awtomatlaşdyrma, Ynamdar hyzmat, Döwrebap internet sahypalary, Mobile programma üpjünçiligi",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/sapchozgut",
+    "type": "organisation",
+    // "img": SapCozgut
+  },
+
+  {
+    "path": "contact",
+    "title": "Habarlaşmak üçin",
+    "description": "Biz bilen habarlaşmak üçin...",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/contact",
+    "type": "contact",
+    // "img": ""
+  },
+
+  {
+    "path": "akhasap",
+    "title": "Akhasap",
+    "description": "Akhasap, Akhasap gurnamak, Akhasap hyzmatlaryna ýardam bermek ",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/akhasap",
+    "type": "desktop program",
+    // "img": Akhasap
+  },
+
+  {
+    "path": "saphasap",
+    "title": "Saphasap",
+    "description": "",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/saphasap",
+    "type": "desktop program",
+    // "img": ""
+  },
+
+  {
+    "path": "sapsargyt",
+    "title": "Sapsargyt",
+    "description": "Söwda agentligi üçin programma üpjünçiligi. Söwda aluw-satuw hyzmatlaryny awtomatlaşdyrma",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/sapsargyt",
+    "type": "mobile application",
+    // "img": ""
+  },
+
+  {
+    "path": "hilli",
+    "title": "Hilli",
+    "description": "Ýokary hilli kompýuter hyzmatlary, kompýuterleri şeýle hem kompýutere degişli gurallar bilen üpjün edilen",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/hilli",
+    "type": "website",
+    // "img": ""
+  },
+
+  {
+    "path": "lomaysowda",
+    "title": "Lomaý Söwda",
+    "description": "Lomaý söwdalar amatly bahadan amala aşyrylýar",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/ls",
+    "type": "website",
+    // "img": ""
+  },
+
+  {
+    "path": "service",
+    "title": "Kompýuter hyzmatlary",
+    "description": "Ýokary hilli service hyzmatlaryny Sap Çözgüt kompaniýasy size üpjün eder.",
+    "keywords": "Sap Chozgut, Sap Hasap, Akhasap, Sap Sargyt, Hilli, Ls, Service",
+    "url": "https://saphasap.com/service",
+    "type": "service",
+    "img": ""
+  }
+]
+
+module.exports = routes_list;
